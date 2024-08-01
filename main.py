bff5bd3c3153302bc16536cd249cedafccf2614a
志翔 李 <2490737433@qq.com>
2024-08-01 22:09:19 +0800

    知乎找热搜

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-import http.cookiejar, urllib.request
+import pprint
 
-cookie = http.cookiejar.CookieJar()
-handler = urllib.request.HTTPCookieProcessor(cookie)
-opener = urllib.request.build_opener(handler)
-response = opener.open('http://www.baidu.com')
-for item in cookie:
-    print(item.name+"="+item.value)
+import requests
+import re,bs4
+
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36'
+}
+url="https://www.zhihu.com/explore"
+res= requests.get(url, headers=headers)
+res.raise_for_status()
+soup = bs4.BeautifulSoup(res.text,'html.parser')
+a=soup.select('a')
+for a1 in a:
+    print(a1.getText())
